83cefc06330a29e4e2da965be6551dbd019f43c8
KELVIN WAMBUA NZIOKI <kelvinwambua069@gmail.com>
2022-07-18 21:26:26 +0300

    name change

--- challenges/views.py
+++ challenges/views.py
@@ -2,7 +2,18 @@ from django.http import HttpRequest
 from django.shortcuts import render
 from django.http import HttpResponse
 
+import challenges
+
 # Create your views here.
 
-def index(request):
-    return HttpResponse("This works!")
+def january(request):
+    return HttpResponse("Eat no meat for the entire month!")
+
+def february(request):
+    return HttpResponse("Walk at least 20 minutes every day!")
+
+def march(request):
+    return HttpResponse("Learn Django for at least 20 minutes every day!")
+
+def monthly_challenge(request, month):
+    return HttpResponse()
