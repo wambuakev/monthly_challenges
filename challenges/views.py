23a3ed2367e7517e96070c9789f2924bcfaf1cda
KELVIN WAMBUA NZIOKI <kelvinwambua069@gmail.com>
2022-07-19 09:23:01 +0300

    name change

--- challenges/views.py
+++ challenges/views.py
@@ -1,19 +1,17 @@
 from django.http import HttpRequest
 from django.shortcuts import render
-from django.http import HttpResponse
-
-import challenges
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
 
-def january(request):
-    return HttpResponse("Eat no meat for the entire month!")
-
-def february(request):
-    return HttpResponse("Walk at least 20 minutes every day!")
-
-def march(request):
-    return HttpResponse("Learn Django for at least 20 minutes every day!")
-
 def monthly_challenge(request, month):
-    return HttpResponse()
+    challenge_text = None
+    if month == 'january':
+        challenge_text = "Eat no meat for the entire month!"
+    if month == 'february':
+        challenge_text = "Walk at least 20 minutes everyday!"
+    elif month == "march":
+        challenge_text = "Learn Django for at least 20 minutes everyday!"
+    else:
+        return HttpResponseNotFound("This month is not supported")
+    return HttpResponse(challenge_text)
